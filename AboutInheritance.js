1a78e35cd10ab07f097b7cdd7b994fe0f1f375c7
vincent chan <vincent@vincentchan.vc>
2013-12-30 16:36:53 -0800

    some koans progress

--- AboutInheritance.js
+++ AboutInheritance.js
@@ -18,29 +18,55 @@ function SwedishChef(age, hobby, mood) {
 
 SwedishChef.prototype = new Muppet();
 
-describe("About inheritance", function() {
-  beforeEach(function(){
-    this.muppet = new Muppet(2, "coding");
-	this.swedishChef = new SwedishChef(2, "cooking", "chillin");
-  });
+function Astronaut(age, hobby, mood){
+	Muppet.call(this, age, hobby);
+	this.mood = mood;
+	
+	this.spacewalk = function(){
+		return age+" year old spacewalking";
+	}
+}
 
-  it("should be able to call a method on the derived object", function() {
-    expect(this.swedishChef.cook()).toEqual("Mmmm soup!");
-  });
+Astronaut.prototype = new Muppet();
 
-  it("should be able to call a method on the base object", function() {
-    expect(this.swedishChef.answerNanny()).toEqual("Everything's cool!");
-  });
+this.muppet = new Muppet(2, "coding");
+this.swedishChef = new SwedishChef(2, "cooking", "chillin");
 
-  it("should set constructor parameters on the base object", function() {
-    expect(this.swedishChef.age).toEqual(2);
-    expect(this.swedishChef.hobby).toEqual("cooking");
-  });
+this.astronaut = new Astronaut(22,"spacewalking", "chillin");
+this.astronaut2 = new Astronaut(42,"music", "chillin");
 
-  it("should set constructor parameters on the derived object", function() {
-    expect(this.swedishChef.mood).toEqual("chillin");
-  });
-});
+console.log(this.swedishChef.cook());
+
+console.log(this.astronaut.answerNanny());
+console.log(this.astronaut.age);
+console.log(this.astronaut.spacewalk());
+console.log(this.astronaut2.hobby);
+
+
+
+// describe("About inheritance", function() {
+  // beforeEach(function(){
+    // this.muppet = new Muppet(2, "coding");
+	// this.swedishChef = new SwedishChef(2, "cooking", "chillin");
+  // });
+
+  // it("should be able to call a method on the derived object", function() {
+    // expect(this.swedishChef.cook()).toEqual("Mmmm soup!");
+  // });
+
+  // it("should be able to call a method on the base object", function() {
+    // expect(this.swedishChef.answerNanny()).toEqual("Everything's cool!");
+  // });
+
+  // it("should set constructor parameters on the base object", function() {
+    // expect(this.swedishChef.age).toEqual(2);
+    // expect(this.swedishChef.hobby).toEqual("cooking");
+  // });
+
+  // it("should set constructor parameters on the derived object", function() {
+    // expect(this.swedishChef.mood).toEqual("chillin");
+  // });
+// });
 
 // http://javascript.crockford.com/prototypal.html
 Object.prototype.beget = function () {
@@ -58,28 +84,47 @@ function Gonzo(age, hobby, trick) {
   }
 }
 
+function Hacker(age, hobby, hack) {
+	Muppet.call(this, age, hobby);
+	this.hack = hack;
+	this.doHack = function(){
+		return this.hack+" hacking";
+	}
+}
+
 //no longer need to call the Muppet (base type) constructor
 Gonzo.prototype = Muppet.prototype.beget();
 
-describe("About Crockford's inheritance improvement", function() {
-  beforeEach(function(){
-  this.gonzo = new Gonzo(3, "daredevil performer", "eat a tire");
-  });
+Hacker.prototype = Muppet.prototype.beget();
 
-  it("should be able to call a method on the derived object", function() {
-    expect(this.gonzo.doTrick()).toEqual("eat a tire");
-  });
 
-  it("should be able to call a method on the base object", function() {
-    expect(this.gonzo.answerNanny()).toEqual("Everything's cool!");
-  });
+this.hacker = new Hacker(25, "always testing" , "koans");
+otherHacker = new Hacker(15, "genius", "facebook");
 
-  it("should set constructor parameters on the base object", function() {
-    expect(this.gonzo.age).toEqual(3);
-    expect(this.gonzo.hobby).toEqual("daredevil performer");
-  });
+console.log(this.hacker.hobby);
+console.log(this.hacker.doHack());
+console.log(otherHacker.doHack());
+console.log(otherHacker.age+" year old "+otherHacker.hobby);
 
-  it("should set constructor parameters on the derived object", function() {
-    expect(this.gonzo.trick).toEqual("eat a tire");
-  });
-});
+// describe("About Crockford's inheritance improvement", function() {
+  // beforeEach(function(){
+  // this.gonzo = new Gonzo(3, "daredevil performer", "eat a tire");
+  // });
+
+  // it("should be able to call a method on the derived object", function() {
+    // expect(this.gonzo.doTrick()).toEqual("eat a tire");
+  // });
+
+  // it("should be able to call a method on the base object", function() {
+    // expect(this.gonzo.answerNanny()).toEqual("Everything's cool!");
+  // });
+
+  // it("should set constructor parameters on the base object", function() {
+    // expect(this.gonzo.age).toEqual(3);
+    // expect(this.gonzo.hobby).toEqual("daredevil performer");
+  // });
+
+  // it("should set constructor parameters on the derived object", function() {
+    // expect(this.gonzo.trick).toEqual("eat a tire");
+  // });
+// });
